0ef748723d406515494c147452f73c57877d1e7b
Ivo Leist <ivo.leist@googlemail.com>
2023-05-01 13:45:48 +0200

    updated dependencies

--- convertPheno_server/tests/conftest.py
+++ convertPheno_server/tests/conftest.py
@@ -72,7 +72,6 @@ def mock_client(mocker):
     mocker.patch("server.security.get_public_key", return_value=FAKE_PUBLIC_KEY)
     mocker.patch("jwt.decode", return_value=FAKE_PAYLOAD)
 
-    # with app.test_request_context():FAKE_PUBLIC_KEY
     yield app.test_client()
 
     db.drop_all()
