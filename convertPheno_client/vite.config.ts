79affd6db68f42c489328f130f8a90631c0c6bad
Ivo Leist <ivo.leist@googlemail.com>
2023-05-01 18:33:21 +0200

    correctly instrument code but code coverage is still 0%

--- convertPheno_client/vite.config.ts
+++ convertPheno_client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { createHtmlPlugin as html } from "vite-plugin-html";
+import istanbul from 'vite-plugin-istanbul';
 
 export default defineConfig({
   server: {
@@ -11,6 +12,13 @@ export default defineConfig({
     html({
       entry: "/src/main.jsx",
       template: "index.html",
+    }),
+    istanbul({
+      include: '/src/code/**/*',
+      exclude: ['node_modules', 'test/'],
+      extension: [ '.js', '.jsx', '.tsx' ],
+      requireEnv: true,
+      cypress: true,
     }),
   ],
 });
