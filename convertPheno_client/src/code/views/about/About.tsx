9756152ff7d9a0aa4604772f6de91448969e3a71
Ivo Leist <ivo.leist@googlemail.com>
2023-06-23 18:34:51 +0200

    proof of concept added clickable images

--- convertPheno_client/src/code/views/about/About.tsx
+++ convertPheno_client/src/code/views/about/About.tsx
@@ -13,11 +13,13 @@
 import React from "react";
 import {
   Box,
+  ButtonBase,
   Card,
   CardContent,
   CardMedia,
   Grid,
   Link,
+  Paper,
   Typography,
 } from "@mui/material";
 
@@ -54,6 +56,39 @@ const logos = [
       "https://www.ihi.europa.eu/sites/default/files/styles/teaser_image_horizontal/public/projects/logos/3TR_logo_final.jpg?itok=fDmi4lFR",
   },
 ];
+
+const ImageComponent = ({ src, alt, href }) => (
+  <Grid item>
+    <a href={href}>
+      <ButtonBase>
+        <Paper elevation={2}>
+          <img
+            src={src}
+            alt={alt}
+            style={{ width: "100%", display: "block" }}
+          />
+        </Paper>
+      </ButtonBase>
+    </a>
+  </Grid>
+);
+
+const CenteredImageButtons = () => (
+  <Box display="flex" justifyContent="center" alignItems="center">
+    <Grid container spacing={2} justifyContent="center">
+      <ImageComponent
+        src="https://lh4.googleusercontent.com/--THiaUxpKwM/AAAAAAAAAAI/AAAAAAAAAAA/FpbvQedtiXw/s44-p-k-no-ns-nd/photo.jpg"
+        alt="Image 1"
+        href="https://www.cnag.crg.eu/"
+      />
+      <ImageComponent
+        src="https://www.ihi.europa.eu/sites/default/files/styles/teaser_image_horizontal/public/projects/logos/3TR_logo_final.jpg?itok=fDmi4lFR"
+        alt="Image 2"
+        href="https://www.3tr-imi.eu/"
+      />
+    </Grid>
+  </Box>
+);
 
 export default function About() {
   return (
@@ -96,49 +131,51 @@ export default function About() {
         ))}
       </Grid>
       <br />
-      <Grid
-        container
-        spacing={4}
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Grid item xs={12} sm={6} md={4}>
-          <Card>
-            <CardContent>
-              <Link
-                component="button"
-                variant="h5"
-                onClick={() => {
-                  window.open(
-                    "https://cnag-biomedical-informatics.github.io/convert-pheno/about/",
-                    "_blank",
-                    "noopener"
-                  );
-                }}
-              >
-                Acknowledgements
-              </Link>
-            </CardContent>
-            <Grid container spacing={4}>
-              {logos.map((logo, index) => (
-                <Link key={index} href={logo.url}>
-                  <Grid item xs={12} sm={6} key={index}>
+
+      <Grid item xs={12} sm={12}>
+        <Link
+          component="button"
+          variant="h5"
+          onClick={() => {
+            window.open(
+              "https://cnag-biomedical-informatics.github.io/convert-pheno/about/",
+              "_blank",
+              "noopener"
+            );
+          }}
+        >
+          Acknowledgements
+        </Link>
+      </Grid>
+      <br />
+      <Grid item xs={12} sm={12}>
+        <CenteredImageButtons />
+      </Grid>
+
+      {/* <Grid item xs={12} sm={12}>
+        <Grid container spacing={4}>
+          {logos.map((logo, index) => (
+            <Link key={index} href={logo.url}>
+              <Grid item xs={12} sm={5} key={index}>
+                <Card>
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    height="100%"
+                  >
                     <CardMedia
                       component="img"
-                      height="140px"
                       image={logo.image}
-                      alt={logo.name}
+                      title={logo.name}
                     />
-                  </Grid>
-                </Link>
-              ))}
-            </Grid>
-          </Card>
+                  </Box>
+                </Card>
+              </Grid>
+            </Link>
+          ))}
         </Grid>
-      </Grid>
+      </Grid> */}
     </Box>
   );
 }
