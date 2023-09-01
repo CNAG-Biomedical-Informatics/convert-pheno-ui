54669cbbff1ca688957380424a8bec8760d7883a
Ivo Leist <ivo.leist@googlemail.com>
2023-09-01 19:31:09 +0200

    generating the OPCS-4 link is working

--- convertPheno_client/src/code/views/conversion/ components/clinicalData/components/dataGrid/CopyOnClick.jsx
+++ convertPheno_client/src/code/views/conversion/ components/clinicalData/components/dataGrid/CopyOnClick.jsx
@@ -50,6 +50,20 @@ const getFieldAndToolTipValues = (data, field) => {
   };
 };
 
+function stringToHash(string) {
+  var hash = 0;
+
+  if (string.length == 0) return hash;
+
+  for (i = 0; i < string.length; i++) {
+    char = string.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash;
+  }
+
+  return hash;
+}
+
 const getValue = (props) => {
   const { data, colDef } = props;
   const field = colDef.field;
