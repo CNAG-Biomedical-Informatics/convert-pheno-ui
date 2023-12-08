ed2f7cde46d572776c6a3ca524e2f2adddc26df0
Ivo Leist <ivo.leist@googlemail.com>
2023-12-08 14:45:01 +0100

    removed extra Axios File

--- convertPheno_client/src/code/apis.js
+++ convertPheno_client/src/code/apis.js
@@ -10,8 +10,28 @@
   License: GPL-3.0 license
 */
 
-import axiosInstance from "./AxiosInstance";
 import auth from "./Auth";
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+})
+
+axiosInstance.interceptors.request.use(config => {
+  if (window.location.hostname.endsWith('.github.dev') ) {
+    console.log('Running in Codespaces');
+    config.headers['X-Github-Token'] = import.meta.env.VITE_GITHUB_TOKEN;
+  }
+  console.log('config', config);
+  return config;
+}, error => {
+  console.log('error', error);
+  return Promise.reject(error);
+});
+
 
 const baseUrl =
   process.env.NODE_ENV === "production"
